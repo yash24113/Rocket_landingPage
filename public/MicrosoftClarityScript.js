47f2305c2b822ed5173f61a6b324650ef679e041
yash24113 <yashkharva506@gmail.com>
2025-07-14 14:08:36 +0530

    add MicrosoftClarityScript

--- public/MicrosoftClarityScript.js
+++ public/MicrosoftClarityScript.js
@@ -10,7 +10,7 @@ export default function MicrosoftClarityScript() {
           c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
           t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
           y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", process.env.Microsoft_Clarity_ID);`
+        })(window, document, "clarity", "script", '${process.env.Microsoft_Clarity_ID}');`
       }}
     />
   );
