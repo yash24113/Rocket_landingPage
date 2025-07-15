045ea78e9a2f3ca9289e112013509b5b5c9148df
yash24113 <yashkharva506@gmail.com>
2025-07-15 16:57:03 +0530

    SEO Performance Fix

--- src/app/metadata.js
+++ src/app/metadata.js
@@ -1,4 +1,4 @@
 export const metadata = {
-  title: "Rocket Landing Page",
-  description: "Welcome to Rocket Landing Page – your one-stop solution for premium sarees and ethnic wear. Discover our exclusive collection now!",
+  title: "Amrita Global Enterprises",
+  description: "Welcome to Amrita Global Enterprises – your one-stop solution for premium sarees and ethnic wear. Discover our exclusive collection now!",
 }; 
