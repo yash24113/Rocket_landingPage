7fbde3b971363119bcaf3b23186e7b7815c8c020
yash24113 <yashkharva506@gmail.com>
2025-07-15 17:02:21 +0530

    SEO Performance Fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,16 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="icon" href="https://amritafashions.com/wp-content/uploads/amrita-fashions-company-logo-150x150.webp" sizes="32x32" />
+        {/* Defer main CSS to avoid render-blocking */}
+        <link
+          rel="stylesheet"
+          href="/_next/static/css/styles.css"
+          media="print"
+          onLoad="this.media='all'"
+        />
+        <noscript>
+          <link rel="stylesheet" href="/_next/static/css/styles.css" />
+        </noscript>
       </Head>
       <body>
         <Main />
